db656e6a6a0154bf0c674bb1350f353ee6ef19e5
Harry Pierson <harry@devhawk.net>
2015-07-08 00:31:32 -0700

    cleanup using statements

--- Startup.cs
+++ Startup.cs
@@ -1,17 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Diagnostics;
-using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http;
-using Microsoft.AspNet.Routing;
-using Microsoft.Framework.Configuration;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.Logging;
-using Microsoft.Framework.Logging.Console;
-using Microsoft.Framework.Runtime;
 
 namespace HawkProto2
 {
