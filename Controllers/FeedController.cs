d7013f0373c3046aa69a40e0f4ccf2e18f97d146
Harry Pierson <harry@devhawk.net>
2015-07-09 00:37:04 -0700

    added RSS feed support

--- Controllers/FeedController.cs
+++ Controllers/FeedController.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Linq;
+using System.Text;
+using System.Xml;
 using Microsoft.AspNet.Mvc;
 
 namespace HawkProto2
@@ -25,20 +28,69 @@ namespace HawkProto2
         
         public IActionResult Rss()
         {
-            //  redirect /feed/rss to /feed
-            return Content("FeedController.Rss");
+            Response.ContentType = "application/rss+xml";
+            
+            var rootUrl = new Uri("http://devhawk.net");
+            
+            using (var writer = new HttpResponseStreamWriter(Response.Body, Encoding.UTF8))
+            {
+                var settings = new XmlWriterSettings()
+                {
+                    Indent = true,
+                    NewLineHandling = NewLineHandling.Entitize,
+                };
+
+                using (var xw =  XmlWriter.Create(writer, settings))
+                {
+                    xw.WriteStartElement("rss");
+                    xw.WriteAttributeString("version", "2.0");
+                    xw.WriteStartElement("channel");
+                    xw.WriteElementString("title", "DevHawk");
+                    xw.WriteElementString("link", rootUrl.ToString());
+                    xw.WriteElementString("description", "Passion * Technology * Ruthless Competence");
+                    
+                    foreach (var post in _repo.Posts().Take(10))
+                    {
+                        xw.WriteStartElement("item");
+                        xw.WriteElementString("title", post.Title);
+                        
+                        var postRelUrl = Url.Action("Post", "Home", new { year = post.Date.Year, month = post.Date.Month, day = post.Date.Day, slug = post.Slug });
+                        var postAbsUrl = new Uri(rootUrl, postRelUrl);
+                        xw.WriteElementString("link", postAbsUrl.ToString());
+                        xw.WriteElementString("author", post.Author.Name); // TODO: author should be email address
+                        xw.WriteElementString("pubdate", post.Date.ToString("r"));
+                        foreach (var cat in post.Categories)
+                        {
+                            xw.WriteElementString("category", cat.Title);
+                        }
+                        foreach (var tag in post.Tags)
+                        {
+                            xw.WriteElementString("category", tag.Title);
+                        }
+                        xw.WriteStartElement("content", "encoded", "http://purl.org/rss/1.0/modules/content/");
+                        xw.WriteCData(post.RenderedContent.Value);
+                        xw.WriteEndElement(); // content:encoded
+                        xw.WriteEndElement(); // item
+                    }
+                    
+                    xw.WriteEndElement(); // channel
+                    xw.WriteEndElement(); // rss
+                }
+            }
+
+            return new EmptyResult();
         }
 
-        public IActionResult Atom()
-        {
-            return Content("FeedController.Atom");
-        }
-        
-        [RouteAttribute("atom")]
-        public IActionResult RootAtom()
-        {
-            //redirect to /feed/atom
-            return Atom();
-        }
+        //  public IActionResult Atom()
+        //  {
+        //      return Content("FeedController.Atom");
+        //  }
+        //  
+        //  [RouteAttribute("atom")]
+        //  public IActionResult RootAtom()
+        //  {
+        //      //redirect to /feed/atom
+        //      return Atom();
+        //  }
     }
 }
