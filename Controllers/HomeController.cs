ba3ef281b08f3c226d91128a3fd8312aac578b17
Harry Pierson <harry@devhawk.net>
2015-07-08 19:20:34 -0700

    added post collection pages support

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Framework.Logging;
 using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Routing;
 
 namespace HawkProto2
 {
@@ -10,15 +12,22 @@ namespace HawkProto2
         const int PAGE_SIZE = 5;
 
         private readonly IPostRepository _repo;
+        private readonly ILogger _logger;
         
-        public HomeController(IPostRepository repo)
+        public HomeController(IPostRepository repo, ILoggerFactory loggerFactory)
         {
             if (repo == null)
             {
                 throw new ArgumentNullException(nameof(repo));
             }
+
+            if (loggerFactory == null)
+            {
+                throw new ArgumentNullException(nameof(loggerFactory));
+            }
             
             this._repo = repo;
+            this._logger = loggerFactory.CreateLogger(nameof(HomeController));
         }
 
         [Route("")]
@@ -26,32 +35,42 @@ namespace HawkProto2
         {
             return IndexPage(1);
         }
-                
+        
+        private IActionResult PostsHelper(IEnumerable<Post> posts, int pageNum, string action, object routeValues = null)
+        {
+            // if the user asks for a page less than or equal to zero, redirect to the first page
+            if (pageNum <= 0)
+            {
+                return RedirectToAction(action);
+            }
+             
+            // if there are no posts, return 404
+            var postCount = posts.Count();
+            if (postCount == 0)
+            {
+                return new HttpNotFoundResult();
+            }
+            
+            // if the user asks for a page beyond the last page, redirect to the last page
+            var pageCount = postCount / PAGE_SIZE + (postCount % PAGE_SIZE == 0 ? 0 : 1);
+            if (pageNum > pageCount)
+            {
+                var routeValueDict = new RouteValueDictionary(routeValues);
+                routeValueDict.Add("pageNum", pageCount);
+
+                return RedirectToAction(action + "Page", routeValueDict);
+            }
+            
+            var skip = (pageNum - 1) * PAGE_SIZE;
+            var pagePosts = posts.Skip(skip).Take(PAGE_SIZE).ToArray();
+            
+            return View("Index", pagePosts);
+        }   
+                      
         [Route("/page/{pageNum}")]
         public IActionResult IndexPage(int pageNum)
         {
-            int skip = 0;
-            if (pageNum > 0)
-            {
-                skip = (pageNum - 1) * PAGE_SIZE;
-            }
-            else
-            {
-                return RedirectToAction("Index");
-            }
-            
-            var posts = _repo.AllPosts().Skip(skip).Take(PAGE_SIZE).ToArray();
-            var totalPostCount = _repo.AllPosts().Count();
-            var lastPage = totalPostCount / PAGE_SIZE + (totalPostCount % PAGE_SIZE == 0 ? 0 : 1);
-            
-            if (totalPostCount == 0)
-                return new HttpNotFoundResult();
-            
-            if (posts.Length == 0)
-            {
-                return RedirectToAction("IndexPage", new { pageNum = lastPage });
-            }    
-            return View("Index", posts);
+            return PostsHelper(_repo.AllPosts(), pageNum, "Index");
         }
         
         [Route("archive")]
@@ -76,37 +95,38 @@ namespace HawkProto2
         [Route("{year:int}/page/{pageNum:int}")]
         public IActionResult PostsByYearPage(int year, int pageNum)
         {
-            //TODO add date constraints
-            return Content($"HomeController.PostsByYear {year} page #{pageNum}");
+            return PostsHelper(_repo.AllPosts().Where(p => p.Date.Year == year), pageNum, "PostsByYear", new { year = year });
         }
 
-        [Route("{year:int}/{month:int}")]
+        [Route("{year:int}/{month:range(1,12)}")]
         public IActionResult PostsByMonth(int year, int month)
         {
             return PostsByMonthPage(year, month, 1);
         }
 
-        [Route("{year:int}/{month:int}/page/{pageNum}")]
+        [Route("{year:int}/{month:range(1,12)}/page/{pageNum}")]
         public IActionResult PostsByMonthPage(int year, int month, int pageNum)
         {
-            //TODO add date constraints
-            return Content($"HomeController.PostsByMonth {year}-{month} page #{pageNum}");
+            return PostsHelper(
+                _repo.AllPosts().Where(p => p.Date.Year == year && p.Date.Month == month), 
+                pageNum, "PostsByMonth", new { year = year, month = month });
         }
 
-        [Route("{year:int}/{month:int}/{day:int}")]
+        [Route("{year:int}/{month:range(1,12)}/{day:range(1,31)}")]
         public IActionResult PostsByDay(int year, int month, int day)
         {
             return PostsByDayPage(year, month, day, 1);
         }
 
-        [Route("{year:int}/{month:int}/{day:int}/page/{pageNum}")]
+        [Route("{year:int}/{month:range(1,12)}/{day:range(1,31)}/page/{pageNum}")]
         public IActionResult PostsByDayPage(int year, int month, int day, int pageNum)
         {
-            //TODO add date constraints
-            return Content($"HomeController.PostsByDay {year}-{month}-{day} page #{pageNum}");
+            return PostsHelper(
+                _repo.AllPosts().Where(p => p.Date.Year == year && p.Date.Month == month && p.Date.Day == day), 
+                pageNum, "PostsByMonth", new { year = year, month = month, day = day });
         }
 
-        [Route("{year:int}/{month:int}/{day:int}/{slug}")]
+        [Route("{year:int}/{month:range(1,12)}/{day:range(1,31)}/{slug}")]
         public IActionResult Post(int year, int month, int day, string slug)
         {
             //TODO add date constraints
@@ -129,7 +149,9 @@ namespace HawkProto2
         [Route("category/{name}/page/{pageNum}")]
         public IActionResult CategoryPage(string name, int pageNum)
         {
-            return Content($"HomeController.Category {name} page #{pageNum}");
+            return PostsHelper(
+                _repo.AllPosts().Where(p => p.Categories.Any(c => c.Slug == name)), 
+                pageNum, "Category", new { name = name });
         }
         
         [Route("tag/{name}")]
@@ -141,7 +163,9 @@ namespace HawkProto2
         [Route("tag/{name}/page/{pageNum}")]
         public IActionResult TagPage(string name, int pageNum)
         {
-            return Content($"HomeController.Tag {name} page #{pageNum}");
+            return PostsHelper(
+                _repo.AllPosts().Where(p => p.Tags.Any(c => c.Slug == name)), 
+                pageNum, "Tag", new { name = name });
         }
         
         [Route("author/{name}")]
@@ -153,7 +177,9 @@ namespace HawkProto2
         [Route("author/{name}/page/{pageNum}")]
         public IActionResult AuthorPage(string name, int pageNum)
         {
-            return Content($"HomeController.Author {name} page #{pageNum}");
+            return PostsHelper(
+                _repo.AllPosts().Where(p => p.Author.Slug == name), 
+                pageNum, "Author", new { name = name });
         }
         
     }
