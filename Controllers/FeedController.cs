484e7d82e86d0304a9bf5954c1fbfb57f67ca5ff
Harry Pierson <harry@devhawk.net>
2015-07-11 13:25:42 -0700

    Added Atom Feed support + updated RSS feed in order to validate

--- Controllers/FeedController.cs
+++ Controllers/FeedController.cs
@@ -48,6 +48,12 @@ namespace HawkProto2
                     xw.WriteElementString("title", "DevHawk");
                     xw.WriteElementString("link", rootUrl.ToString());
                     xw.WriteElementString("description", "Passion * Technology * Ruthless Competence");
+
+                    xw.WriteStartElement("atom", "link", "http://www.w3.org/2005/Atom");
+                    xw.WriteAttributeString("href", new Uri(rootUrl, Url.Action("Rss", "Feed")).ToString());
+                    xw.WriteAttributeString("rel", "self");
+                    xw.WriteAttributeString("type", "application/rss+xml");
+                    xw.WriteEndElement(); // atom:link
                     
                     foreach (var post in _repo.Posts().Take(10))
                     {
@@ -57,8 +63,9 @@ namespace HawkProto2
                         var postRelUrl = Url.Action("Post", "Home", new { year = post.Date.Year, month = post.Date.Month, day = post.Date.Day, slug = post.Slug });
                         var postAbsUrl = new Uri(rootUrl, postRelUrl);
                         xw.WriteElementString("link", postAbsUrl.ToString());
-                        xw.WriteElementString("author", post.Author.Name); // TODO: author should be email address
-                        xw.WriteElementString("pubdate", post.Date.ToString("r"));
+                        xw.WriteElementString("guid", postAbsUrl.ToString());
+                        xw.WriteElementString("author", $"harry@devhawk.net ({post.Author.Name})"); // TODO: author should be email address
+                        xw.WriteElementString("pubDate", post.Date.ToString("r"));
                         foreach (var cat in post.Categories)
                         {
                             xw.WriteElementString("category", cat.Title);
@@ -81,16 +88,86 @@ namespace HawkProto2
             return new EmptyResult();
         }
 
-        //  public IActionResult Atom()
-        //  {
-        //      return Content("FeedController.Atom");
-        //  }
-        //  
-        //  [RouteAttribute("atom")]
-        //  public IActionResult RootAtom()
-        //  {
-        //      //redirect to /feed/atom
-        //      return Atom();
-        //  }
+        [RouteAttribute("atom")]
+        public IActionResult RootAtom()
+        {
+            return Atom();
+        }
+
+        public IActionResult Atom()
+        {
+            Response.ContentType = "application/atom+xml";
+            
+            var rootUrl = new Uri("http://devhawk.net");
+            
+            using (var writer = new HttpResponseStreamWriter(Response.Body, Encoding.UTF8))
+            {
+                var settings = new XmlWriterSettings()
+                {
+                    Indent = true,
+                    NewLineHandling = NewLineHandling.Entitize,
+                };
+
+
+                using (var xw =  XmlWriter.Create(writer, settings))
+                {
+                    var latestPost = _repo.Posts().Take(10).OrderByDescending(p => p.DateModified).First();
+                    
+                    xw.WriteStartElement(null, "feed", "http://www.w3.org/2005/Atom");
+                    xw.WriteElementString(null, "id", "http://www.w3.org/2005/Atom", rootUrl.ToString());
+                    xw.WriteElementString(null, "title", "http://www.w3.org/2005/Atom", "DevHawk");
+                    xw.WriteElementString(null, "updated", "http://www.w3.org/2005/Atom", latestPost.DateModified.ToString("o"));
+                    xw.WriteStartElement(null, "link", "http://www.w3.org/2005/Atom");
+                    xw.WriteAttributeString("rel", "self");
+                    xw.WriteAttributeString("href", new Uri(rootUrl, Url.Action("Atom", "Feed")).ToString());
+                    xw.WriteEndElement(); // link
+                    foreach (var post in _repo.Posts().Take(10))
+                    {
+                        var postRelUrl = Url.Action("Post", "Home", new { year = post.Date.Year, month = post.Date.Month, day = post.Date.Day, slug = post.Slug });
+                        var postAbsUrl = new Uri(rootUrl, postRelUrl);
+
+                        xw.WriteStartElement(null, "entry", "http://www.w3.org/2005/Atom");
+
+                        xw.WriteElementString(null, "id", "http://www.w3.org/2005/Atom", postAbsUrl.ToString());
+                        xw.WriteElementString(null, "title", "http://www.w3.org/2005/Atom", post.Title);
+                        xw.WriteElementString(null, "updated", "http://www.w3.org/2005/Atom", post.DateModified.ToString("o"));
+
+                        xw.WriteStartElement(null, "link", "http://www.w3.org/2005/Atom");
+                        xw.WriteAttributeString("rel", "alternate");
+                        xw.WriteAttributeString("href", postRelUrl.ToString());
+                        xw.WriteEndElement(); // link
+
+                        xw.WriteStartElement(null, "author", "http://www.w3.org/2005/Atom");
+                        xw.WriteElementString(null, "name", "http://www.w3.org/2005/Atom", post.Author.Name);
+                        xw.WriteEndElement(); // link
+
+                        foreach (var cat in post.Categories)
+                        {
+                            xw.WriteStartElement(null, "category", "http://www.w3.org/2005/Atom");
+                            xw.WriteAttributeString("term", cat.Slug);
+                            xw.WriteAttributeString("label", cat.Title);
+                            xw.WriteEndElement(); // category
+                        }
+                        foreach (var tag in post.Tags)
+                        {
+                            xw.WriteStartElement(null, "category", "http://www.w3.org/2005/Atom");
+                            xw.WriteAttributeString("term", tag.Slug);
+                            xw.WriteAttributeString("label", tag.Title);
+                            xw.WriteEndElement(); // category
+                        }
+                           
+                        xw.WriteStartElement(null, "content", "http://www.w3.org/2005/Atom");
+                        xw.WriteAttributeString("type", "html");
+                        xw.WriteValue(post.Content());
+                        xw.WriteEndElement(); // content
+                        xw.WriteEndElement(); // entry
+                    }
+
+                    xw.WriteEndElement(); // feed
+                }
+            }
+            
+            return new EmptyResult();
+        }
     }
 }
