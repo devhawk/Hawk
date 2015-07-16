285f72c6df795fee7feaa8a957d75f2acef99e0f
Harry Pierson <harry@devhawk.net>
2015-07-16 15:44:43 -0700

    moved not found code to seperate middleware class

---
+++ Middleware/NotFoundMiddleware.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Logging;
+
+namespace HawkProto2
+{
+	public class NotFoundMiddleware
+	{
+	    RequestDelegate _next;
+	
+	    public NotFoundMiddleware(RequestDelegate next)
+	    {
+	        _next = next;
+	    }
+		
+	    public async Task Invoke(HttpContext context)
+	    {
+			var loggerFactory = context.ApplicationServices.GetRequiredService<ILoggerFactory>();
+			var logger = loggerFactory.CreateLogger("NotFoundMiddleware");
+
+			await _next(context);
+			
+            if (context.Response.StatusCode == 404)
+            {
+                // if the request path looks like /yyyy/mm/dd/slug or /slug, redirect to the blog controller
+    			var match = Regex.Match(context.Request.Path.Value ?? string.Empty, @"^/(?:(?<year>\d\d\d\d)/(?<month>\d\d?)/(?<day>\d\d?)/)?(?<slug>[_a-z0-0-]*)$", RegexOptions.IgnoreCase);
+                if (match.Success)
+                {
+                    logger.LogInformation("{Path}{QueryString} looks like a WP era URL. Redirecting to /blog{Path}", context.Request.Path, context.Request.QueryString, context.Request.Path);
+
+                    // TODO: Lookup post in repository before redirecting 
+
+                    context.Response.Redirect("/blog" + context.Request.Path, true);
+                }
+                else
+                {
+                    logger.LogWarning("{Path}{QueryString} Not Found", context.Request.Path, context.Request.QueryString);
+                }
+            }
+
+		}
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -22,28 +22,8 @@ namespace HawkProto2
             loggerFactory.MinimumLevel = LogLevel.Information;
             loggerFactory.AddConsole();
             
-            var logger = loggerFactory.CreateLogger("404 Tracker");
-            app.Use (next => async context =>
-            {
-                await next(context);
-                
-                if (context.Response.StatusCode == 404)
-                {
-                    // if the request path looks like /yyyy/mm/dd/slug or /slug, redirect to the blog controller
-        			var match = Regex.Match(context.Request.Path.Value ?? string.Empty, @"^/(\d\d\d\d/\d\d?/\d\d?/)?[_a-z0-0-]*$", RegexOptions.IgnoreCase);
-                    if (match.Success)
-                    {
-                        logger.LogInformation("{Path}{QueryString} looks like a WP era URL. Redirecting to /blog{Path}", context.Request.Path, context.Request.QueryString, context.Request.Path);
-                        context.Response.Redirect("/blog" + context.Request.Path, true);
-                    }
-                    else
-                    {
-                        logger.LogWarning("{Path}{QueryString} Not Found", context.Request.Path, context.Request.QueryString);
-                    }
-                }
-            });
-            
             app.UseMiddleware<DasBlogRedirector>();
+            app.UseMiddleware<NotFoundMiddleware>();
             app.UseMvcWithDefaultRoute();
         }
     }
