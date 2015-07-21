94ee20de112794014ecc0fe561856688c856c168
Harry Pierson <harry@devhawk.net>
2015-07-20 18:42:20 -0700

    Add ConfigBuilder support

--- Startup.cs
+++ Startup.cs
@@ -1,13 +1,26 @@
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Diagnostics;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Framework.Configuration;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.Logging;
+using Microsoft.Framework.Runtime;
 using Azure = Microsoft.WindowsAzure.Storage;
 
 namespace HawkProto2
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
+        {
+            // Setup configuration sources.
+            var builder = new ConfigurationBuilder(appEnv.ApplicationBasePath)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; set; }
+
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
@@ -16,14 +29,25 @@ namespace HawkProto2
             //  services.AddInstance<IPostRepository>(AzurePostRepository.GetRepository(Azure.CloudStorageAccount.DevelopmentStorageAccount));
         }
 
-        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.MinimumLevel = LogLevel.Information;
             loggerFactory.AddConsole();
             
+            // Use the error page only in development environment.
+            if (env.IsDevelopment())
+            {
+                app.UseErrorPage(ErrorPageOptions.ShowAll);
+            }
+            else
+            {
+                // Add Error handling middleware which catches all application specific errors and
+                // send the request to the following path or controller action.
+                //  app.UseErrorHandler("/Home/Error");
+            }
+
             app.UseMiddleware<DasBlogRedirector>();
             app.UseMiddleware<NotFoundMiddleware>();
-            app.UseErrorPage(ErrorPageOptions.ShowAll);
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
         }
