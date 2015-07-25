42cd28cfbcbd3f11026b1a95a3abae8537ac1c92
Harry Pierson <harry@devhawk.net>
2015-07-25 09:36:15 -0700

    variety of client stuff * removed bootstrap from bower - using CDN version * removed preen from package, added a variety of gulp tools * modified gulpfile to convert/concat/minimize and clean css * moved to custom icon font * _layout changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,35 @@
 /// <binding Clean='clean' />
 'use strict';
-var gulp = require("gulp"),
-  preen = require('preen');
+var gulp = require("gulp");
+var less = require('gulp-less');
+var path = require('path');
+var concat = require("gulp-concat");
+var cssmin = require("gulp-cssmin");
+var rimraf = require("rimraf");
+var project = require("./project.json");
 
-gulp.task('preen', function(cb) {
-  preen.preen({}, cb);
+var paths = {
+  webroot: "./" + project.webroot + "/"
+};
+
+paths.vsCss = paths.webroot + "bower/highlight.js/src/styles/vs.css";
+paths.css = paths.webroot + "css/**/*.css";
+paths.minCss = paths.webroot + "css/**/*.min.css";
+paths.concatCssDest = paths.webroot + "css/hawk.min.css";
+ 
+gulp.task('clean:css', function(cb) {
+    rimraf(paths.css, cb);
 });
+
+gulp.task('less', function () {
+  return gulp.src('.\\wwwroot\\css\\*.less', { base: "." })
+    .pipe(less())
+    .pipe(gulp.dest('.'));    
+});
+
+gulp.task('css', ["clean:css", "less"], function() {
+    gulp.src([paths.css, paths.vsCss, "!" + paths.minCss])
+      .pipe(concat(paths.concatCssDest))
+      .pipe(cssmin())
+      .pipe(gulp.dest("."));
+})
