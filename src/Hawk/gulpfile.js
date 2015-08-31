4979df9e7d5fee87cb0d744ff67d8ce956ad8879
Harry Pierson <hpierson@microsoft.com>
2015-08-31 12:56:00 -0700

    Config file cleanup

--- src/Hawk/gulpfile.js
+++ src/Hawk/gulpfile.js
@@ -9,8 +9,8 @@ var del = require('del');
 var project = require("./project.json");
 
 var paths = {
-  webroot: "./" + project.webroot + "/",
-  outputWebroot: "./bin/output/" + project.webroot + "/"
+    webroot: "./" + project.webroot + "/",
+    outputWebroot: "./bin/output/" + project.webroot + "/"
 };
 
 paths.css = paths.webroot + "css/**/*.css";
@@ -18,22 +18,22 @@ paths.minCss = paths.webroot + "css/**/*.min.css";
 paths.concatCssDest = paths.webroot + "css/hawk.min.css";
 paths.outputCssFolder = paths.outputWebroot + "css/";
 paths.outputMinCss = paths.outputWebroot + "css/**/*.min.css";
- 
-gulp.task('clean:css', function(cb) {
+
+gulp.task('clean:css', function (cb) {
     del(paths.css, cb);
 });
 
-gulp.task('output:css', function(cb) {
-  del([paths.outputCssFolder + "*.css", paths.outputCssFolder + "*.less", "!" + paths.outputMinCss], cb);
+gulp.task('output:css', function (cb) {
+    del([paths.outputCssFolder + "*.css", paths.outputCssFolder + "*.less", "!" + paths.outputMinCss], cb);
 });
 
 gulp.task('less', function () {
-  return gulp.src('.\\wwwroot\\css\\*.less', { base: "." })
-    .pipe(less())
-    .pipe(gulp.dest('.'));    
+    return gulp.src('.\\wwwroot\\css\\*.less', { base: "." })
+      .pipe(less())
+      .pipe(gulp.dest('.'));
 });
 
-gulp.task('css', ["clean:css", "less"], function() {
+gulp.task('css', ["clean:css", "less"], function () {
     gulp.src([paths.css, "!" + paths.minCss])
       .pipe(concat(paths.concatCssDest))
       .pipe(cssmin())
